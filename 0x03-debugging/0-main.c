6b49f7f98ecfe51cbf15ed1a922645ff19b5fb66
WanjiruKinyua <maureentally@gmail.com>
2023-03-23 12:30:43 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -11,21 +11,7 @@ int main(void)
 
         i = 0;
         positive_or_negative(i);
-	return (0);
+
+        return (0);
 }
 
-/**
- * positive_or_negative - prints if integer is positive or negative
- * @i: the integer to check
- *
- * Return: no return value
- */
-void positive_or_negative(int i)
-{
-    if (i > 0)
-        printf("%d is positive\n", i);
-    else if (i < 0)
-        printf("%d is negative\n", i);
-    else
-        printf("0 is zero\n");
-}
