345c423742460848fdf879955945a48c6bdb6781
WanjiruKinyua <maureentally@gmail.com>
2023-03-28 15:24:56 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,11 +20,10 @@ int main(void)
 			if (i != 98 || j != 99)
 			{
 				putchar(',');
-				putchar(' ");
+				putchar(' ');
 			}
 		}
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
