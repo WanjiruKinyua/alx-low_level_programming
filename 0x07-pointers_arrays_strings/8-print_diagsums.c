5bf40c4f2dcd3d614389af4042a87d04703c8145
WanjiruKinyua <maureentally@gmail.com>
2023-04-04 12:50:22 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,18 +6,18 @@
  *
  * Return: void
  */
-void print_diagsums(int *a, int size) 
+void print_diagsums(int *a, int size)
 {
 	int i, j, sum1 = 0, sum2 = 0;
 
 	/* Calculate the sum of the main diagonal and the secondary diagonal*/
 	for (i = 0; i < size; i++)
 	{
-	sum1 += *(a + i*size + i);
+	sum1 += *(a + i * size + i);
 	}
 	for (j = 0; j < size; j++)
 	{
-	sum2 += *(a + j*size + (size-1-j));
+	sum2 += *(a + j * size + (size - 1 - j));
 	}
 
 	/* Print the sum of the two diagonals*/
