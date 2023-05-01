cbe5c543f11409f9ddc58d5b6d725104088415aa
WanjiruKinyua <maureentally@gmail.com>
2023-05-01 16:51:26 -0700

    update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,30 +12,31 @@
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int i, j, k, len;
+	int i, j, k, len = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-
-	len = 0;
 	for (i = 0; i < ac; i++)
 	{
-		if (av[i] == NULL)
-			return (NULL);
-		len += strlen(av[i]) + 1; /* add 1 for the newline character */
+		for (j = 0; av[i][j]; j++)
+			len++;
+		len++;
 	}
 
-	str = malloc(sizeof(char) * len);
+	str = malloc(len * sizeof(char) + 1);
+
 	if (str == NULL)
 		return (NULL);
 
 	k = 0;
+
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 			str[k++] = av[i][j];
-				str[k++] = '\n';
+		str[k++] = '\n';
 	}
+
 	str[k] = '\0';
 
 	return (str);
