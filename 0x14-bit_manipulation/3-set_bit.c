7b99ee096a27060b10533ab088882400b04850b5
WanjiruKinyua <maureentally@gmail.com>
2023-08-03 15:23:53 -0700

    update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,13 +8,13 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask;
+	unsigned long int check = 1;
 	
-	if (index >= sizeof(unsigned long int) * 8)
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 
-	mask = 1UL << index;
-	*n = *n | mask;
+	check = check << index;
+	*n = *n | check;
 
 	return (1);
 }
